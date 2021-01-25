24598e50a1dfc7cf6caa00f538cc3b9fa33911bf
antonsaf18 <anton23651@gmail.com>
2021-01-25 17:24:29 +0700

    added new pages

--- dist/js/script.js
+++ dist/js/script.js
@@ -55,4 +55,11 @@ burgerButton.onclick = function () {
   burgerButton.classList.toggle('active');
   document.querySelector('.header__menu').classList.toggle('active');
   document.querySelector('body').classList.toggle('lock');
-};
+}; //Активный пункт меню с помощью JavaScript
+
+
+document.querySelectorAll('.header__list li a').forEach(function (el) {
+  if (window.location.pathname.indexOf(el.getAttribute('href')) > -1) {
+    el.classList.add('active-menu__link');
+  }
+});
