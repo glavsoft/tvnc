46e26c753a543e7be7dbdfd65f4636a131846dde
antonsaf18 <anton23651@gmail.com>
2021-02-20 18:10:17 +0700

    added new style

--- dist/js/script.js
+++ dist/js/script.js
@@ -58,25 +58,26 @@ burgerButton.onclick = function () {
 
 
 document.querySelectorAll('.header__list li a').forEach(function (el) {
-  if (window.location.pathname.indexOf(el.getAttribute('href')) > -1) {
+  if (window.location.pathname.indexOf(el.getAttribute('href')) > 0) {
     //по умолчанию было -1
     el.classList.add('active-menu__link');
-  } // else if ( window.location.pathname.indexOf(el.getAttribute('href')) > -1 && window.location.pathname === '/') {    // ('/') второе условие нужно для того что бы исключить корневой файл
-  //     el.classList.add('active-menu__link');
-  // }
-  // else if ( window.location.pathname.indexOf(el.getAttribute('href')) > -1 && window.location.pathname === '/index.html') {  // ('/index.html')
-  //     el.classList.add('active-menu__link');
-  // }
-  //console.log(window.location.pathname);
+  } else if (window.location.pathname.indexOf(el.getAttribute('href')) > -1 && window.location.pathname === '/') {
+    // ('/') второе условие нужно для того что бы исключить корневой файл
+    el.classList.add('active-menu__link');
+  } else if (window.location.pathname.indexOf(el.getAttribute('href')) > -1 && window.location.pathname === '/index.html') {
+    // ('/index.html')
+    el.classList.add('active-menu__link');
+  } //console.log(window.location.pathname);
 
-}); // //Активный пункт САМОГО ВЕРХНЕГО меню с помощью JavaScript
-// document.querySelectorAll(".header__list li a").forEach(function(el1){
-//     //console.log(el1.getAttribute('class'));
-//     if (el1.getAttribute('class') == 'sub-menu__link active-menu__link') {
-//         //console.log(el1.parentElement.innerHTML);
-//         //console.log('sadadsasda');
-//         //console.log(el1.parentNode.parentNode.parentNode.innerHTML);
-//         let punktMainMenu = el1.parentNode.parentNode.parentNode.querySelector('.header__link');
-//         punktMainMenu.classList.add('active-menu__link');
-//     }
-// });
+}); //Активный пункт САМОГО ВЕРХНЕГО меню с помощью JavaScript
+
+document.querySelectorAll(".header__list li a").forEach(function (el1) {
+  //console.log(el1.getAttribute('class'));
+  if (el1.getAttribute('class') == 'sub-menu__link active-menu__link') {
+    //console.log(el1.parentElement.innerHTML);
+    //console.log('sadadsasda');
+    //console.log(el1.parentNode.parentNode.parentNode.innerHTML);
+    var punktMainMenu = el1.parentNode.parentNode.parentNode.querySelector('.header__link');
+    punktMainMenu.classList.add('active-menu__link');
+  }
+});
