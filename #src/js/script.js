b0907e00766245c1b0fa3e37428221a18ea2cce4
antonsaf18 <anton23651@gmail.com>
2021-04-01 15:38:18 +0700

    fixed 404 page

--- #src/js/script.js
+++ #src/js/script.js
@@ -1,7 +1,6 @@
 @@include('../components/cookie/script.js')
 @@include('../components/nav/script.js')
 @@include('../components/form/script.js')
-@@include('../components/404/script.js')
-
+@@include('../pages/404/script.js')
 
 
